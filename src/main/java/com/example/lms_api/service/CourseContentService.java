2c1df8e4fbbc1b42ee4dc6245f7ce896055b99ba
Giang <giang1111100@gmail.com>
2026-05-26 11:39:31 +0700

    add api course content

--- src/main/java/com/example/lms_api/service/CourseContentService.java
+++
@@ -1,7 +0,0 @@
-package com.example.lms_api.service;
-
-import com.example.lms_api.dto.response.CourseContentResponse;
-
-public interface CourseContentService {
-    CourseContentResponse getCourseContent(Integer courseId);
-}
